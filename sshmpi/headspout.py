49625f5c2e53996bdc683dc81b7d2a9c748c5384
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2020-04-16 21:49:51 -0600

    Made headspout read in length-message pairs.

--- sshmpi/headspout.py
+++ sshmpi/headspout.py
@@ -25,9 +25,36 @@ async def spout():
         sys.stdout.flush()
 
 
+async def stdin_read():
+    """ Continously reads bytes from stdout and forwards them to SSHMPI HNP. """
+    _, writer = await asyncio.open_connection("127.0.0.1", 8888)
+    logging.info("Successfully connected to server.")
+    buf = b""
+    while 1:
+        logging.info("Reading from stdin.")
+        sys.stdout.flush()
+        # Read the length of the message given in 16 bytes.
+        buf += sys.stdin.buffer.read(16)
+
+        # Parse the message length bytes.
+        blength = buf
+        length = int(blength.decode("ascii"))
+        logging.info("Decoded length: %d", length)
+        sys.stdout.flush()
+        writer.write(buf)
+
+        # Read the message proper.
+        buf = sys.stdin.buffer.read(length + 1)
+        writer.write(buf)
+        logging.info("Message of length %d written to server.", length)
+
+        # Reset buffer.
+        buf = b""
+
+
 def main() -> None:
     """ Reads from stdin and echos bytes to head TCP listener. """
-    asyncio.run(spout())
+    asyncio.run(stdin_read())
 
 
 if __name__ == "__main__":
