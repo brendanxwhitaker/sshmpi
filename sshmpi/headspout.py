23985d6d31dd3ebc550a7ec9c3f3c663b2790ee9
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2020-04-17 09:28:00 -0600

    Changer servertype of heaspout to socketserver.

--- sshmpi/headspout.py
+++ sshmpi/headspout.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """ Called by workers on the head node via SSH. Broadcasts stdout to TCP. """
 import sys
+import socket
 import asyncio
 import logging
 
@@ -17,7 +18,7 @@ async def spout():
         buf = b""
         while 1:
             buf = sys.stdin.buffer.read(BUFFER_SIZE)
-            logging.info("Sending: %s" % buf.decode())
+            logging.info("Sending: %s", buf.decode())
             writer.write(buf)
 
     except KeyboardInterrupt:
@@ -27,30 +28,33 @@ async def spout():
 
 async def stdin_read():
     """ A version of the above function which explicitly reads the length bytes. """
+    ip = "127.0.0.1"
+    port = 8888
     logging.info("Attempting to open connection to server.")
-    _, writer = await asyncio.open_connection("127.0.0.1", 8888)
-    logging.info("Successfully connected to server.")
-    buf = b""
-    while 1:
-        logging.info("Reading from stdin.")
-        sys.stdout.flush()
-        # Read the length of the message given in 16 bytes.
-        buf += sys.stdin.buffer.read(16)
-
-        # Parse the message length bytes.
-        blength = buf
-        length = int(blength.decode("ascii"))
-        logging.info("Decoded length: %d", length)
-        sys.stdout.flush()
-        writer.write(buf)
-
-        # Read the message proper.
-        buf = sys.stdin.buffer.read(length + 1)
-        writer.write(buf)
-        logging.info("Message of length %d written to server.", length)
-
-        # Reset buffer.
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ip, port))
+        logging.info("Successfully connected to server.")
         buf = b""
+        while 1:
+            logging.info("Reading from stdin.")
+            sys.stdout.flush()
+            # Read the length of the message given in 16 bytes.
+            buf += sys.stdin.buffer.read(16)
+
+            # Parse the message length bytes.
+            blength = buf
+            length = int(blength.decode("ascii"))
+            logging.info("Decoded length: %d", length)
+            sys.stdout.flush()
+            sock.sendall(buf)
+
+            # Read the message proper.
+            buf = sys.stdin.buffer.read(length + 1)
+            sock.sendall(buf)
+            logging.info("Message of length %d written to server.", length)
+
+            # Reset buffer.
+            buf = b""
 
 
 def main() -> None:
