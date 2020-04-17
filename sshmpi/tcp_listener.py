3f8ee786f8a61adc870552529f9b5e74fb19512d
Brendan Whitaker <whitaker.213@osu.edu>
2020-04-16 22:19:29 -0600

    Added tcp listener (missing from last commit).

--- sshmpi/tcp_listener.py
+++ sshmpi/tcp_listener.py
@@ -1,5 +1,6 @@
 """ A simple TCP server listener. """
 import sys
+import pickle
 import asyncio
 import logging
 import functools
@@ -8,6 +9,7 @@ from multiprocessing.connection import Connection
 
 logging.basicConfig(filename="server-head.log", level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
 
 async def handle_echo(reader, writer, funnel):
     """ Echoes any data sent to the server back to the sender. """
@@ -23,9 +25,33 @@ async def handle_echo(reader, writer, funnel):
         logging.info("Sent message into head funnel: %s", message)
 
 
+async def stream_read(reader, writer, funnel):
+    buf = b""
+    while 1:
+        # Read the length of the message given in 16 bytes.
+        buf += reader.read(16)
+
+        # Parse the message length bytes.
+        blength = buf
+        length = int(blength.decode("ascii"))
+        logging.info("SERVER: Decoded length: %s" % length)
+        sys.stdout.flush()
+
+        # Read the message proper.
+        buf = reader.read(length + 1)
+
+        # Deserialize the data and send to the backward connection client.
+        obj = pickle.loads(buf)
+        funnel.send(obj)
+        logging.info("SERVER: Object sent: %s" % str(obj))
+
+        # Reset buffer.
+        buf = b""
+
+
 async def wait_for_data(funnel: Connection):
     """ Runs a persistent server. """
-    callback = functools.partial(handle_echo, funnel=funnel)
+    callback = functools.partial(stream_read, funnel=funnel)
     server = await asyncio.start_server(callback, "127.0.0.1", 8888)
 
     addr = server.sockets[0].getsockname()
