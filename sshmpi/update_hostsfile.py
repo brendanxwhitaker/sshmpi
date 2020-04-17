91172c47c86048c97d0762adbcdc29c7d87bdf62
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2020-04-17 09:03:20 -0600

    Added print statements to hostsfile script, and sorted the hosts.

--- sshmpi/update_hostsfile.py
+++ sshmpi/update_hostsfile.py
@@ -4,16 +4,18 @@ from sshmpi.utils import get_available_hostnames_from_sshconfig
 
 
 def main():
-    hosts = get_available_hostnames_from_sshconfig()
+    hosts = sorted(get_available_hostnames_from_sshconfig())
     ip = socket.gethostbyname("0.tcp.ngrok.io")
     hostline = " ".join(hosts)
     line = "%s\t" % ip + " ".join(hosts)
     with open("/etc/hosts", "r") as hosts_file:
         lines = hosts_file.read().split("\n")
         if hostline in lines:
+            print("Found existing hosts line. Exiting.")
             sys.exit()
     with open("/etc/hosts", "a") as hosts_file:
         hosts_file.write(line + "\n")
+        print("Wrote to hostsfile: %s" % line)
 
 
 if __name__ == "__main__":
