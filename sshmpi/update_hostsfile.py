13bcdc33b9ed478ad648e3398a636eb49bb24a69
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2020-04-17 09:00:24 -0600

    Added check to make sure hostsfile hasn't already been written.

--- sshmpi/update_hostsfile.py
+++ sshmpi/update_hostsfile.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 from sshmpi.utils import get_available_hostnames_from_sshconfig
 
@@ -5,10 +6,14 @@ from sshmpi.utils import get_available_hostnames_from_sshconfig
 def main():
     hosts = get_available_hostnames_from_sshconfig()
     ip = socket.gethostbyname("0.tcp.ngrok.io")
-    line = "%s\t" % ip + " ".join(hosts) + "\n"
-    print(line)
+    hostline = " ".join(hosts)
+    line = "%s\t" % ip + " ".join(hosts)
+    with open("/etc/hosts", "r") as hosts_file:
+        lines = hosts_file.read().split("\n")
+        if hostline in lines:
+            sys.exit()
     with open("/etc/hosts", "a") as hosts_file:
-        hosts_file.write(line)
+        hosts_file.write(line + "\n")
 
 
 if __name__ == "__main__":
