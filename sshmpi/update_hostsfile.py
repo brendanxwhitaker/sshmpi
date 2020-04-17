80a53754474c7adfe3570e4551317d533a3ae78f
Brendan Whitaker <whitaker.213@osu.edu>
2020-04-17 14:56:18 +0000

    Finished hostsfile script.

--- sshmpi/update_hostsfile.py
+++ sshmpi/update_hostsfile.py
@@ -1,8 +1,15 @@
-import subprocess
+import socket
 from sshmpi.utils import get_available_hostnames_from_sshconfig
 
 
 def main():
     hosts = get_available_hostnames_from_sshconfig()
-    p = subprocess.Popen(["ping", "0.tcp.ngrok.io"], stdout=subprocess.PIPE)
-    print(p.stdout)
+    ip = socket.gethostbyname("0.tcp.ngrok.io")
+    line = "%s\t" % ip + " ".join(hosts) + "\n"
+    print(line)
+    with open("/etc/hosts", "a") as hosts_file:
+        hosts_file.write(line)
+
+
+if __name__ == "__main__":
+    main()
