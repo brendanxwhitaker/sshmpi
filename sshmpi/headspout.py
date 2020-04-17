f58f1741afab050b255efbbbe880675f769b3ebc
Brendan Whitaker <whitaker.213@osu.edu>
2020-04-16 21:36:52 -0600

    Added logging to headspout.

--- sshmpi/headspout.py
+++ sshmpi/headspout.py
@@ -2,18 +2,22 @@
 """ Called by workers on the head node via SSH. Broadcasts stdout to TCP. """
 import sys
 import asyncio
+import logging
 
 BUFFER_SIZE = 1024
+
+logging.basicConfig(filename="client-head.log", level=logging.DEBUG)
 
 
 async def spout():
     """ Continously reads bytes from stdout and forwards them to SSHMPI HNP. """
     _, writer = await asyncio.open_connection("127.0.0.1", 8888)
+    logging.info("Successfully connected to server.")
     try:
         buf = b""
         while 1:
             buf = sys.stdin.buffer.read(BUFFER_SIZE)
-            print("Sending: %s" % buf.decode())
+            logging.info("Sending: %s" % buf.decode())
             writer.write(buf)
 
     except KeyboardInterrupt:
