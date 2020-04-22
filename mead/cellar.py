efbc16ca8c4696cd70c21bf898b7238013fa4490
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2020-04-21 18:45:18 -0600

    Changed client out mp object to a queue.
    This handles multiple producers properly.

--- mead/cellar.py
+++ mead/cellar.py
@@ -1,11 +1,12 @@
 """ Storage for ``mead``. """
 from typing import Set, Dict, List
+import multiprocessing as mp
 from multiprocessing.connection import Connection
 
 PIPE_COUNTER = 0
 HOSTNAMES: List[str] = []
 USED_PIPE_IDS: Set[str] = set()
-HEAD_FUNNELS: Dict[str, Connection] = {}
+HEAD_QUEUES: Dict[str, mp.Queue] = {}
 HEAD_SPOUTS: Dict[str, Connection] = {}
 INTERNAL_FUNNELS: Dict[str, Connection] = {}
 INTERNAL_SPOUTS: Dict[str, Connection] = {}
