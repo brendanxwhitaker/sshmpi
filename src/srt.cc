ba6f7b964031ca55f47d0b2ee2605a1ead1966aa
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2020-06-29 20:35:03 -0600

    Added working rendezvous function.

--- src/srt.cc
+++ src/srt.cc
@@ -37,67 +37,6 @@ int mead_startup()
     return 0;
 }
 
-
-int mead_rendezvous(const char* remote_addr, int port)
-{
-    // These are the socket objects/identifiers, which are just integers.
-    int socket, status;
-
-    // Will contain address and port.
-    struct sockaddr_in remote_sockaddr;
-    struct sockaddr_in local_sockaddr;
-
-    printf("srt socket\n");
-    socket = srt_create_socket();
-    if (socket == SRT_ERROR)
-    {
-        fprintf(stderr, "srt_socket: %s\n", srt_getlasterror_str());
-        return 1;
-    }
-
-    // ==========================================================
-    // ================ INITIALIZE SOCKADDRESS ==================
-    printf("srt remote address\n");
-    remote_sockaddr.sin_family = AF_INET;
-
-    // Set the remote port.
-    remote_sockaddr.sin_port = htons(port);
-
-    // Load the remote IP address into the ``sockaddr_in`` object.
-    if (inet_pton(AF_INET, remote_addr, &remote_sockaddr.sin_addr) != 1)
-    {
-        return 1;
-    }
-    // ================ INITIALIZE SOCKADDRESS ==================
-    // ==========================================================
-
-    // ==========================================================
-    // ================ INITIALIZE SOCKADDRESS ==================
-    printf("srt local address\n");
-    local_sockaddr.sin_family = AF_INET;
-
-    // Set the local port.
-    local_sockaddr.sin_port = htons(port);
-
-    // Load the local IP address into the ``sockaddr_in`` object.
-    if (inet_pton(AF_INET, INADDR_ANY, &local_sockaddr.sin_addr) != 1)
-    {
-        return 1;
-    }
-    // ================ INITIALIZE SOCKADDRESS ==================
-    // ==========================================================
-
-    printf("srt rendezvous\n");
-    status = srt_rendezvous(socket, (struct sockaddr*)&local_sockaddr, sizeof local_sockaddr, (struct sockaddr*)&remote_sockaddr, sizeof remote_sockaddr);
-    if (status == SRT_ERROR)
-    {
-        fprintf(stderr, "srt_rendezvous: %s\n", srt_getlasterror_str());
-        return 1;
-    }
-
-    return socket;
-}
-
 int mead_sendmsg2(int socket, const char* message)
 {
     // These are the socket objects/identifiers, which are just integers.
@@ -107,10 +46,27 @@ int mead_sendmsg2(int socket, const char* message)
     status = srt_sendmsg2(socket, message, sizeof message, NULL);
     if (status == SRT_ERROR)
     {
-        fprintf(stderr, "srt_sendmsg: %s\n", srt_getlasterror_str());
+        fprintf(stderr, "srt_sendmsg2: %s\n", srt_getlasterror_str());
         return 1;
     }
     return 0;
+}
+
+char* mead_recvmsg2(int socket, int buffer_size)
+{
+    // These are the socket objects/identifiers, which are just integers.
+    int status;
+    
+    char *message = (char*)malloc(buffer_size + 1);
+    strcpy(message, "SRT_ERROR");
+
+    printf("srt recvmesg2 <<\n");
+    status = srt_recvmsg2(socket, message, sizeof message - 1, NULL);
+    if (status == SRT_ERROR)
+    {
+        fprintf(stderr, "srt_recvmsg2: %s\n", srt_getlasterror_str());
+    }
+    return message;
 }
 
 int mead_close(int socket)
@@ -135,6 +91,71 @@ int mead_cleanup()
     return 0;
 }
 
+int mead_rendezvous(const char* remote_addr, int remote_port, int local_port)
+{
+    int ss, st;
+    struct sockaddr_in lsa;
+    struct sockaddr_in rsa;
+    int yes = 1;
+    struct sockaddr_storage their_addr;
+
+
+    srt_setloglevel(LOG_DEBUG);
+
+    printf("srt startup\n");
+    srt_startup();
+
+    printf("srt socket\n");
+    ss = srt_create_socket();
+    if (ss == SRT_ERROR)
+    {
+        fprintf(stderr, "srt_socket: %s\n", srt_getlasterror_str());
+        return 1;
+    }
+
+    printf("srt bind address\n");
+    lsa.sin_family = AF_INET;
+    lsa.sin_port = htons(remote_port);
+    if (inet_pton(AF_INET, "127.0.0.1", &lsa.sin_addr) != 1)
+    {
+        return 1;
+    }
+
+    printf("srt bind address\n");
+    rsa.sin_family = AF_INET;
+    rsa.sin_port = htons(remote_port);
+    if (inet_pton(AF_INET, remote_addr, &rsa.sin_addr) != 1)
+    {
+        return 1;
+    }
+
+    printf("srt setsockflag\n");
+    SRT_TRANSTYPE tt = SRTT_FILE;
+    srt_setsockflag(ss, SRTO_RCVSYN, &yes, sizeof yes);
+    srt_setsockflag(ss, SRTO_SENDER, &yes, sizeof yes);
+    srt_setsockflag(ss, SRTO_TRANSTYPE, &tt, sizeof tt);
+    srt_setsockflag(ss, SRTO_MESSAGEAPI, &yes, sizeof yes);
+    srt_setsockopt(ss, 0, SRTO_RENDEZVOUS, &yes, sizeof yes);
+
+    printf("srt bind\n");
+    st = srt_bind(ss, (struct sockaddr*)&lsa, sizeof lsa);
+    if (st == SRT_ERROR)
+    {
+        fprintf(stderr, "srt_bind: %s\n", srt_getlasterror_str());
+        return 1;
+    }
+
+    printf("srt connect\n");
+    st = srt_connect(ss, (struct sockaddr*)&rsa, sizeof rsa);
+    if (st == SRT_ERROR)
+    {
+        fprintf(stderr, "srt_connect: %s\n", srt_getlasterror_str());
+        return 1;
+    }
+
+    return socket;
+}
+
 PYBIND11_MODULE(pysrt, m) {
     // optional module docstring
     m.doc() = "pybind11 srt plugin";
@@ -143,6 +164,8 @@ PYBIND11_MODULE(pysrt, m) {
     m.def("mead_startup", &mead_startup, "");
     m.def("mead_rendezvous", &mead_rendezvous, "");
     m.def("mead_sendmsg2", &mead_sendmsg2, "");
+    m.def("mead_recvmsg2", &mead_recvmsg2, "");
+    m.def("mead_close", &mead_close, "");
     m.def("mead_cleanup", &mead_cleanup, "");
 
 }
